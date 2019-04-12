ad2c0d636ad38373818acfe5f18ddeeac30e0876
Giuditta <giudittaparolini@gmail.com>
2019-04-12 14:13:13 +0200

    Pie plot finished with aggregated data

--- 2_code_scripts/plot_methods.py
+++ 2_code_scripts/plot_methods.py
@@ -5,39 +5,35 @@ Created on Thu Apr  4 23:14:09 2019
 
 @author: giudittaparolini
 """
-import csv
+
 import seaborn as sns
 import matplotlib.pyplot as plt
-import os
+import utilities as ut
 
-#PLOT HIST LANGUAGES
-def count_plot_lang(df):
-     sns.set(style="darkgrid")
-     sns.set(rc={"figure.figsize":(25,14)})
-     sns.countplot(y="Language", data=df)
-     
-#PLOT HIST OF THE PUBLICATION YEARS
+
+#Plot the histogram for the publications printed in each year
 def hist_pub_year(df):
     plt.figure(figsize=(25,14))
     df.hist(column="Publication Year", bins=50 )
     plt.xlabel("Publication Year",fontsize=15)
     plt.ylabel("Frequency",fontsize=15)
+    
+#Plot the histogram for the publications' languages
+def count_plot_lang(df):
+     sns.set(style="darkgrid")
+     sns.set(rc={"figure.figsize":(25,14)})
+     sns.countplot(y="Language", data=df)
 
-#PLOT PIE CHART LANGUAGES
-def lang_count_list():
-    with open(os.path.join("..", "3_printouts","lang_counts.csv")) as f:
-        reader = csv.reader(f)
-        my_list = list(reader)
-    return my_list
-        
+#Plot pie chart for the publications' languages
 def pie_lang():
-    my_list = lang_count_list()
+    my_list = ut.agg_list()
     languages = dict(my_list).keys()
     counts = dict(my_list).values()
+    
     plt.figure(figsize=(10,8))      
-    explode = (0.1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-    plt.pie(counts, explode,labels=None, 
-            autopct='%1.1f%%', pctdistance=1.1, shadow=True, startangle=140)
+    explode = (0.1,0,0,0,0,0)
+    plt.pie(counts, explode, 
+            autopct='%1.1f%%', pctdistance=0.7, shadow=True, startangle=140)
     plt.title("Publications distributed according to language")
     plt.legend(loc="best", labels=languages )
     plt.axis('equal')
--- 2_code_scripts/untitled0.py
+++ 2_code_scripts/untitled0.py
@@ -1,43 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 25 23:37:36 2019
+Created on Fri Apr 12 10:39:47 2019
 
 @author: giudittaparolini
 """
-
 import os
-import matplotlib.pyplot as plt
 import csv
 import itertools
 
-# PRINT TO A CSV FILE IN THE FOLDER 1_DATA
-def print_csv_data (df, filename):
-    df.to_csv(os.path.join("..", "1_data", filename))
-
-
-#1 PRINT TO A TXT FILE IN THE FOLDER 3_PRINTOUTS
-def print_txt_printouts (to_be_printed, filename):
-    with open(os.path.join("..", "3_printouts", filename),'w') as outfile:
-        for item in to_be_printed:
-            print(item, file=outfile)
-       
-# PRINT TO A CSV FILE IN THE FOLDER 3_PRINTOUTS
-def print_csv_printouts (df, filename):
-    df.to_csv(os.path.join("..", "3_printouts", filename))
-
-# PRINT A JPG FIGURE IN THE FOLDER 4_PLOTS
-def print_plot (fig, filename):
-    plt.savefig(os.path.join("..", "4_plots", filename))
-
-# 
-       
 def lang_count_list():
     with open(os.path.join("..", "3_printouts","lang_counts.csv")) as f:
         reader = csv.reader(f)
         my_list = list(reader)
         return my_list
     
+print(lang_count_list())
 
 def main_lang_list():
     with open(os.path.join("..", "3_printouts","lang_counts.csv")) as f:
@@ -49,6 +27,7 @@ def main_lang_list():
         agg_list.append(item)
     return agg_list
 
+print(main_lang_list())
 
 def other_count():
     my_list = lang_count_list()
@@ -59,6 +38,7 @@ def other_count():
     others = sum(other_count)
     return others
 
+print(other_count())
 
 def agg_list():
     agg_list = []
@@ -71,13 +51,5 @@ def agg_list():
         agg_list.append(item)
     return agg_list
 
+print(agg_list())
 
-     
-        
-#1 STRIP BLANK SPACES FROM ALL COLUMNS OF A DATAFRAME
-def StripAllCols(df): 
-    df.columns = df.columns.str.strip()
-
-#2 STRIP BLANK SPACES FROM A SPECIFIC COLUMN OF A DATAFRAME
-def StripCol(df,column): 
-    df[column] = df[column].str.strip()
